888c56b40294c3b6dd536da37dbfe0dd9284036f
Y56380X <ueberstimmen@gmail.com>
2017-03-08 19:11:22 +0100

    Update to VS2017, example project

---
+++ src/RF24.Examples.CheckModule.Compatible/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+	Copyright (c) 2017 Y56380X
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+namespace Y56380X.RF24.Examples.CheckModule
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var rf24 = new RF24(22, 8, 32); // Use GPIO_22, GPIO_8
+			
+			// Start module
+			rf24.Begin();
+
+			// Print module information
+			rf24.PrintDetails();
+		}
+	}
+}
